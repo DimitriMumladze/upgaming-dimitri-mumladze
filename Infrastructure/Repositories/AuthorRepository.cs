ad73197b1f0e35b961a139d897098b7fc05818a6
DimitriMumladze <kenwo.dm@gmail.com>
2025-10-28 01:17:20 +0400

    Book and Author Repositories Created

---
+++ Infrastructure/Repositories/AuthorRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Entities;
+using Infrastructure.Persistence;
+using Infrastructure.Repositories.BaseRepository;
+
+namespace Infrastructure.Repositories;
+
+public class AuthorRepository : GenericRepository<Author, int>
+{
+    public AuthorRepository(UpgamingDbContext context) : base(context)
+    {
+    }
+}   
+
---
+++ Infrastructure/Repositories/BookRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Infrastructure.Persistence;
+using Infrastructure.Repositories.BaseRepository;
+
+namespace Infrastructure.Repositories;
+
+public class BookRepository : GenericRepository<Book, int>
+{
+    public BookRepository(UpgamingDbContext context) : base(context)
+    {
+    }
+}
