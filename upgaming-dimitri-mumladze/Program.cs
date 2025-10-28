f8e15b09aebae44598ad0917b6565da16ff3278c
DimitriMumladze <kenwo.dm@gmail.com>
2025-10-28 11:02:34 +0400

    Get ready to be in production

--- upgaming-dimitri-mumladze/Program.cs
+++ upgaming-dimitri-mumladze/Program.cs
@@ -3,31 +3,42 @@ using Infrastructure.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddInfrastructure(builder.Configuration);
-builder.Services.AddApplication();  
-
-
-
+builder.Services.AddApplication();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen(c =>
+{
+
+    var xmlFile = $"{System.Reflection.Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    if (File.Exists(xmlPath))
+    {
+        c.IncludeXmlComments(xmlPath);
+    }
+
+    c.AddServer(new Microsoft.OpenApi.Models.OpenApiServer
+    {
+        Url = "https://upgamingapi.resorter360.ge",
+        Description = "Production Server"
+    });
+});
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Enable Swagger in all environments (production included)
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "UpGaming API v1");
+    c.RoutePrefix = "swagger"; // Access at: https://upgamingapi.resorter360.ge/swagger
+    c.DocumentTitle = "UpGaming API Documentation";
+});
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
