6e31b3fb09dd321e730b06463bb2c08164082f46
DimitriMumladze <kenwo.dm@gmail.com>
2025-10-28 08:14:41 +0400

    Added validations for books create and update.

---
+++ Application/Validations/BookValidatos/BookCreateValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.BookFeatures.Commands.CreateBook;
+using FluentValidation;
+
+namespace Application.Validations.BookValidatos;
+
+public class BookCreateValidator : AbstractValidator<CreateBookCommand>
+{
+    public BookCreateValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .WithMessage("Title cannot be null or empty.")
+            .MaximumLength(200)
+            .WithMessage("Title cannot exceed 200 characters.");
+
+        RuleFor(x => x.AuthorId)
+            .GreaterThan(0)
+            .WithMessage("AuthorId must be greater than 0.");
+
+        RuleFor(x => x.PublicationYear)
+            .GreaterThan(1000)
+            .WithMessage("PublicationYear must be a valid year.")
+            .LessThanOrEqualTo(DateTime.Now.Year)
+            .WithMessage("PublicationYear cannot be in the future.");
+    }
+}
---
+++ Application/Validations/BookValidatos/BookUpdateValidator.cs
@@ -0,0 +1,26 @@
+﻿using Application.Features.BookFeatures.Commands.UpdateBook;
+using FluentValidation;
+
+namespace Application.Validations.BookValidatos;
+
+public class BookUpdateValidator : AbstractValidator<UpdateBookCommand>
+{
+    public BookUpdateValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty()
+            .WithMessage("Title cannot be null or empty.")
+            .MaximumLength(200)
+            .WithMessage("Title cannot exceed 200 characters.");
+
+        RuleFor(x => x.AuthorId)
+            .GreaterThan(0)
+            .WithMessage("AuthorId must be greater than 0.");
+
+        RuleFor(x => x.PublicationYear)
+            .GreaterThan(1000)
+            .WithMessage("PublicationYear must be a valid year.")
+            .LessThanOrEqualTo(DateTime.Now.Year)
+            .WithMessage("PublicationYear cannot be in the future.");
+    }
+}
